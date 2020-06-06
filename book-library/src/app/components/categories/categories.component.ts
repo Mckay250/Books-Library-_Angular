597758b3dbe5631af5e49213fe24d31800d6c05d
Mckay17 <kayodeoke4@gmail.com>
2020-06-06 22:28:01 +0100

    tried fixing bug on categories component

--- book-library/src/app/components/categories/categories.component.ts
+++ book-library/src/app/components/categories/categories.component.ts
@@ -9,38 +9,38 @@ import { Router } from '@angular/router';
 })
 export class CategoriesComponent implements OnInit {
   
-  // categories = [];
-  // thisProducts: any[];
-  // selectedCategory;
+  categories = [];
+  thisProducts: any[];
+  selectedCategory;
 
   constructor(private router: Router,
               private bookService: BookService) { }
 
   ngOnInit(): void {
-    // this.bookService.getCategories()
-    //   .subscribe(
-    //     res => {
-    //       this.categories = res;
-    //       this.getbooksByCategory((this.categories[0]).name)
-    //     },
-    //     err => {alert("could not fetch categories");
-    //             console.log(err)}
-    //   );
+    this.bookService.getCategories()
+      .subscribe(
+        res => {
+          this.categories = res;
+          this.getbooksByCategory((this.categories[0]).name)
+        },
+        err => {alert("could not fetch categories");
+                console.log(err)}
+      );
     
   }
 
   getbooksByCategory(category){
-    // this.bookService.getBooksByCategory(category)
-    //     .subscribe(
-    //       res => this.thisProducts = res,
-    //       err => alert("couldn't fetch data" + err)
-    //     )
+    this.bookService.getBooksByCategory(category)
+        .subscribe(
+          res => this.thisProducts = res,
+          err => alert("couldn't fetch data" + err)
+        )
   }
 
   onNewCategoryClick(category){
-    // this.thisProducts = [];
-    // this.getbooksByCategory(category);
-    // this.selectedCategory = category;
+    this.thisProducts = [];
+    this.getbooksByCategory(category);
+    this.selectedCategory = category;
   }
 
   navigateToAdmin() {
