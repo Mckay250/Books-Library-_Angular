4a5d2a65d79d8ccc9869275c7fd96f26370349fb
Mckay17 <kayodeoke4@gmail.com>
2020-06-08 16:15:53 +0100

    generated the book-list comp

--- book-library/src/app/app.module.ts
+++ book-library/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AuthService } from './services/auth.service';
 import { CategoryComponent } from './components/category/category.component';
+import { BookListComponent } from './components/book-list/book-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CategoryComponent
+    CategoryComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule,
